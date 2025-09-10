a58294dacbd0cd490ffb886d111c63bced9fa22a
Trishanth-reddy <strishanthreddy@gmail.com>
2025-09-10 19:43:23 +0530

    first m

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,34 @@ import User from '../models/userModel.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password, patientId } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      patientId,
+      role: 'patient' // Role is always 'patient' for this route
+    });
+
+    await user.save();
+    res.status(201).json({ message: 'Patient created successfully', user });
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
 
 router.post('/login', async (req, res) => {
   try {
