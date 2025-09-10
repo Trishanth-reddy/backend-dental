c51d983d011942391b1fde2e83b9468a8e1e03e3
Trishanth-reddy <strishanthreddy@gmail.com>
2025-09-10 23:05:47 +0530

    firstm

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-app.use(cors());
+app.use(cors({
+  origin: "*", // Allow all origins
+  methods: "GET,POST,PUT,DELETE",
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
