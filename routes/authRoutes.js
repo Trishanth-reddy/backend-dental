9d4523cc9d04ba091886c69def994f12a1d3b6ff
Trishanth-reddy <strishanthreddy@gmail.com>
2025-09-11 10:58:26 +0530

    feat: Add con)

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
-import { protect } from '../middleware/auth.middleware.js';
+import { protect } from '../middleware/auth.Middleware.js';
 
 const router = express.Router();
 
